50320fd6f252cc0117b973dc1442493703479ff8
MegaGG <rhartshorne61@gmail.com>
2018-01-04 17:56:17 +1300

    Add files via upload

---
+++ cmds/DevComs/twitgameset.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, message, args,) => {
+    if (message.author.id === '214662509175504896') {
+        bot.user.setgame("@DeviateESC | New prefix is '?'")
+    }
+}
+
+module.exports.help = {
+    name: "twitgameset"
+}
