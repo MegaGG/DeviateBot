ca6b6f4dcb5b86dfc36e4c288671d5093477fbf4
MegaGG <rhartshorne61@gmail.com>
2018-01-04 17:46:04 +1300

    Update setgame.js

--- cmds/AdminComs/setgame.js
+++ cmds/AdminComs/setgame.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args,) => {
-    if (message.member.author.id === '212073112378933249' || '214662509175504896') {
+    if (message.author.id === '212073112378933249' || '214662509175504896') {
         let game = args.slice(0).join(' ');
         if (!game)
             return message.reply("Field cannot be blank, please enter a game")
