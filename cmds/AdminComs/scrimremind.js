402f956f8e2f4db31d477dfe0fdf114fb12e50c5
MegaGG <rhartshorne61@gmail.com>
2018-01-04 17:18:26 +1300

    Update scrimremind.js

--- cmds/AdminComs/scrimremind.js
+++ cmds/AdminComs/scrimremind.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-    if (message.member.hasPermission("ADMINISTRATOR")) {
+    if (message.member.author.id === '212073112378933249' || '214662509175504896') {
         message.delete();
         message.channel.send("<@&380939853635911680> Reminder that there are scrims tonight! check <#380942440116256768> for more details.");
     }
