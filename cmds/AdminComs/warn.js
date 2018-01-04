09a26ead1e761bb3bc8e2aeb0d9a1eeb406abce8
MegaGG <rhartshorne61@gmail.com>
2018-01-04 17:20:58 +1300

    Update warn.js

--- cmds/AdminComs/warn.js
+++ cmds/AdminComs/warn.js
@@ -1,13 +1,10 @@
 module.exports.run = async (bot, message, args) => {
     if (message.member.author.id === '212073112378933249' || '214662509175504896') {
         let member = message.mentions.members.first();
-        if (!member)
-        return message.reply("Please enter a valid member of the server.");
+        if (!member) return message.reply("Please enter a valid member of the server.");
 
         let reason = args.slice(1).join(' ');
-        if (!reason)
-
-        return message.channel.send(`${member} you have been warned. Reason: None given.`);
+        if (!reason) return message.channel.send(`${member} you have been warned. Reason: None given.`);
 
         message.channel.send(`${member} you have been warned. Reason: ${reason} `)
     } else if (!message.member.hasPermission("ADMINISTRATOR")) {
