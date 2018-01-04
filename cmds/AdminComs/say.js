23aff93034431cf0d3d69e58526da0670fb44900
MegaGG <rhartshorne61@gmail.com>
2018-01-04 17:45:24 +1300

    Update say.js

--- cmds/AdminComs/say.js
+++ cmds/AdminComs/say.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-    if (message.member.author.id === '212073112378933249' || '214662509175504896') {
+    if (message.author.id === '212073112378933249' || '214662509175504896') {
         let text = args.slice(0).join(" ")
         message.delete().catch(O_o=>{});
         message.channel.send(text)
