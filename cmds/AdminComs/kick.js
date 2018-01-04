28ea21531acb1c03887cb940532e4df5506ef792
MegaGG <rhartshorne61@gmail.com>
2018-01-04 17:13:25 +1300

    Create kick.js

---
+++ cmds/AdminComs/kick.js
@@ -0,0 +1,21 @@
+module.exports.run = async (bot, message, args) => {
+    if(message.member.hasPermission("ADMINISTRATOR")) {
+        let member = message.mentions.members.first();
+
+        if(!member) return message.reply("Please mention a valid member of this server");
+        if(!member.kickable) return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+
+        let reason = args.slice(1).join(' ');
+
+        if(!reason) return message.reply("Please indicate a reason for the kick!");
+
+        await member.kick(reason).catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+        message.channel.send(`${member.user.tag} has been kicked by ${message.author.tag}. Reason: ${reason}`);
+    } else {
+        message.reply("Sorry! Command is restricted to Admins only.")
+    }
+}
+
+module.exports.help = {
+    name: "kick"
+}
